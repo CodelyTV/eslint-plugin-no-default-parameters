b1c2d2fd15b2257a1500481f0c88a380ae508dd0
Rafa Gómez <rgomezcasas@gmail.com>
2023-12-12 00:18:49 +0100

    fix: correct ts config

--- src/utils/createRule.ts
+++ src/utils/createRule.ts
@@ -1,5 +1,5 @@
 import { ESLintUtils } from "@typescript-eslint/utils";
 
 export const createRule = ESLintUtils.RuleCreator(
-	() => "https://github.com/CodelyTV/eslint-plugin-hexagonal-architecture/blob/main/README.md",
+	() => "https://github.com/CodelyTV/eslint-plugin-no-default-parameters/blob/main/README.md",
 );
